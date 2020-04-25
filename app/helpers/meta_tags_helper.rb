759be513cbc4c4353b46ce1853e9b6898395b2dd
Baptiste Chebassier <baptistechebassier@MacBook-Pro-de-Baptiste-8.local>
2020-04-25 16:05:06 +0200

    eta og image

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -10,6 +10,15 @@ module MetaTagsHelper
   def meta_image
     meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # little twist to make it work equally with an asset or a url
-    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+    # meta_image.starts_with?("http") ? meta_image : image_url(meta_image) 
   end
+
+  # def meta_image(val = nil)
+  # if val
+  #   @_meta_image = val
+  #   set_meta(:og => { :image => val })
+  #   end
+  # @_meta_image
+  # end
+
 end
